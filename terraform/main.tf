16028cb6b3f39d4082dc32734bb4b381a429de24
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2021-09-15 16:37:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,11 @@ provider "google" {
   # version = "~> 3.45.0"
   project = var.gcp_project_id
   region  = "us-central1"
-  credentials = file(".keyring/mythical-legend-325420-cc65ab9236e1.json")
 }
 
 provider "google-beta" {
   project = var.gcp_project_id
   region = "us-central1"
-  credentials = file(".keyring/mythical-legend-325420-cc65ab9236e1.json")
 }
 
 resource "random_string" "env" {
